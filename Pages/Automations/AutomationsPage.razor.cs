de9262b54150471b84987be9a96f1bc1062bbce4
WsysAllanSampaio <allan.menezes@wsys.inf.br>
2025-01-13 20:27:35 -0300

    Ajustando a rotina de automação de acordo com a descrição inserida

---
+++ Pages/Automations/AutomationsPage.razor.cs
@@ -0,0 +1,39 @@
+﻿using MyTasks.Services.Jira;
+
+namespace MyTasks.Pages.Automations;
+
+public partial class AutomationsPage 
+{
+    [Inject]
+    public HttpClient HttpClient { get; set; }
+
+    public string TaskNumber = "";
+
+    private string taskDescription = "";
+    public string TaskDescription 
+    { 
+        get => taskDescription; 
+        set 
+        {
+            taskDescription = value.Replace("Wsys 2.0 - ", "")
+                                   .Replace("WSYS 2.0 - ", "");
+            TaskType = TaskDescription.ToLower().Contains("padroniza") ? "feat" : "fix";
+            TaskCommentFirstLine = TaskDescription.ToLower().Contains("padroniza") ? PadronizacaoValueText : SegurancaValueText;
+        } 
+    }
+
+    public string TaskCommentFirstLine = "";
+    public string TaskComment = "";
+    public string TaskType = "";
+    public string DataBase = "\"WSYS_APRESENTACAO_12082024_JONATHAN\"\n\n";
+
+    public string PadronizacaoValueText = "Foram feitas as modificações solicitadas.\n\n";
+    public string SegurancaValueText = "Foram feitas as modificações para exibir informações aos usuários de acordo com as suas restrições.\n\n";
+
+    public async Task FindTasks()
+    {
+        JiraService Jira = new();
+
+        await Jira.GetCurrentTask(HttpClient);
+    }
+}
